6347e046bf52834d9f4df9d3b50b33de75b13042
aslanbeyza <aslanbeyza3413@gmail.com>
2025-03-16 05:13:43 +0300

    Plugin

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,10 @@
 import { Elysia,t } from "elysia";
 import { swagger } from "@elysiajs/swagger";
+import { note } from "./note";
 
-
-class Note { 
-  constructor(public data: string[] = ['Beyzoşş']) {} 
-} 
 
 const app = new Elysia()
-
 .use(swagger()) 
-.decorate('note', new Note()) 
-.get('/note', ({ note }) => note.data) 
-.get('/note/:index', ({ note, params:{index}, error }) => {
-  return note.data[index] ?? error(404, 'Not found kardesimm :D'); 
-},
-{
-  params: t.Object({
-    index: t.Number()
-  })
-}
-)
+.use(note)
 .listen(3000); 
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
