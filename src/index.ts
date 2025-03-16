c2165aa8ef10ac09c0693fc53c361e841d85c3ba
aslanbeyza <aslanbeyza3413@gmail.com>
2025-03-16 04:41:03 +0300

    elysia with swagger logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
+import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+//.get("/", () => "Uygulamaya hosgeldiniz efenimm!!!")
+//.get('/hello','Do you miss me')  http://localhost:3000/hello altına götürücek Do you miss me yazısını yazdırıcak
+//.post('/hello', 'Do you miss me beyzoşş?') 
+.use(swagger())  // http://localhost:3000/swagger altına gittiğimizde swagger dökümantasyonunu göstericek
+.get('/', ({ path }) => path) // http://localhost:3000/ altına gittiğimizde / yazısını yazdırıcak
+.get('/hello', ({ path }) => path) // http://localhost:3000/hello altına gittiğimizde /hello yazısını yazdırıcak
+// .post('/hello', 'Do you miss me?')
+
+.listen(3000); 
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
