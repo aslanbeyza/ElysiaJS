c5c0881db005285ac813878a0e9c149ddb4db0f3
aslanbeyza <aslanbeyza3413@gmail.com>
2025-03-16 05:06:58 +0300

    Status code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+import { Elysia,t } from "elysia";
 import { swagger } from "@elysiajs/swagger";
-import { Elysia } from "elysia";
 
 
 class Note { 
@@ -11,9 +11,15 @@ const app = new Elysia()
 .use(swagger()) 
 .decorate('note', new Note()) 
 .get('/note', ({ note }) => note.data) 
-.get('/note/:index', ({ note, params:{index} }) => {
-  return note.data[index]
-})
+.get('/note/:index', ({ note, params:{index}, error }) => {
+  return note.data[index] ?? error(404, 'Not found kardesimm :D'); 
+},
+{
+  params: t.Object({
+    index: t.Number()
+  })
+}
+)
 .listen(3000); 
 
 console.log(
